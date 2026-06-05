7adc3f9e088165f0709c278f15aee57a57990665
pragyann007 <pragyanthapaliya027@gmail.com>
2026-06-05 14:49:18 +0545

    just webhok queee

---
+++ backend/config/redis.js
@@ -0,0 +1,11 @@
+import {Redis} from "ioredis";
+
+export const redisConnection = new Redis({
+    host:"localhost",
+    port:6379,
+    maxRetriesPerRequest:null,
+
+})
+redisConnection.on("connect",()=>{
+    console.log("redis connecteddddd ......")
+})
